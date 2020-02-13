5f4ff54bb3dbc08b5a169a751b1417933b0a9d9d
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-02-13 09:10:18 +0100

    MRG

--- Corona-Energy-Grid-Frontend/src/app/app.module.ts
+++ Corona-Energy-Grid-Frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,9 +9,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatInputModule } from '@angular/material/input';
-
 import { AppComponent } from './app.component';
-import { RoutingModule } from './routing/routing.module'
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -31,7 +28,6 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RoutingModule,
     FormsModule,
     MatButtonModule,
     MatInputModule,
