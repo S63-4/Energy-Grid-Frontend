098cf7f671968c9fd24cfe6d82f4656a4f325455
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-03-24 09:33:36 +0100

    imports

--- Corona-Energy-Grid-Frontend/src/app/REST/http-interceptors/jwt-interceptor.ts
+++ Corona-Energy-Grid-Frontend/src/app/REST/http-interceptors/jwt-interceptor.ts
@@ -1,12 +1,9 @@
 import { Injectable } from "@angular/core";
 import {
-  HttpEvent,
   HttpInterceptor,
   HttpHandler,
   HttpRequest
 } from "@angular/common/http";
-
-import { Observable } from "rxjs";
 import { AuthenticationService } from "../authentication.service";
 
 /** Pass untouched request through to the next request handler. */
